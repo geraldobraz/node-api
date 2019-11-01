0672d85223d9ffa7f23b82b6924cc6fb9f301181
geraldobraz <g.de.almeida.neto@avanade.com>
2019-10-31 21:42:00 -0300

    Adding a person route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const personRoute = require('./routes/person');
+
+app.use((req, res, next) => {
+    console.log(`${new Date().toString()} => ${req.originalUrl}`);
+    next();
+});
+
+app.use(personRoute);
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
---
+++ src/routes/person.js
@@ -0,0 +1,24 @@
+const { Router } = require('express');
+const router = Router();
+
+// QueryString = > query property on the request object
+// localhost:3000/person?name=SOME_NAME
+router.get('/person', (req, res) => {
+    const { name } = req.query;
+    if (name) {
+        res.send(`You have requested a person! ${name}`)
+    } else {
+
+        res.send('You have requested a person!')
+    }
+
+});
+
+// Params property on the request object
+// localhost:3000/person/SOME_NAME
+router.get('/person/:name', (req, res) => {
+    const { name } = req.params;
+    res.send(`You have requested a ${name}!`)
+});
+
+module.exports = router;
