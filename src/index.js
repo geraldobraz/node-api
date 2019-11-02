0eb2801d7af90b04adef8f009ec2ab8d85e7dcd7
geraldobraz <g.de.almeida.neto@avanade.com>
2019-11-02 12:52:50 -0300

    Adding the customer route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const express = require('express');
-
 const app = express();
-
-const personRoute = require('./routes/person');
-
+const personRoute = require('./routes/person.route');
+const customerRoute = require('./routes/customer.route');
 const path = require('path');
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    console.log(`${new Date().toString()} => ${req.originalUrl}`);
+    console.log(`${new Date().toString()} => ${req.originalUrl}`, req.body);
     next();
 });
 
 app.use(personRoute);
+app.use(customerRoute);
+
 app.use(express.static('public'));
 
 // Handler for 404 - Resource Not Found!
@@ -19,11 +23,15 @@ app.use((req, res, next) => {
     res.status(404).send('We think you are lost!')
 });
 // Handler for Error 500
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.sendFile(path.join(__dirname, '../public/500.html'));
 });
 
+
+
+// Starting server
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
--- src/models/customer.model.js
+++ src/models/customer.model.js
@@ -6,7 +6,7 @@ const user = '';
 const password = '';
 
 
-mongoose.connect(`mongodb://localhost:27017/${database}`);
+mongoose.connect(`mongodb://localhost:27017/${database}`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const CustomerSchema = new mongoose.Schema({
     name: String,
